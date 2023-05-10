dce67396d47581ad580ad4cc0f69b354d14cc70f
Roger Pimenta <roger.pimenta@live.com>
2023-05-10 11:54:16 -0300

    Microservices and modules

--- ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderService.java
+++ ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderService.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-public class NewOrderMain {
+public class NewOrderService {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (var orderDispatcher = new KafkaDispatcher<Order>()) {
             try (var emailDispatcher = new KafkaDispatcher<Email>()) {
